6aba41f0c52f43b087e5eada160f380dc9da9b98
Anh Tho <ntat000@gmail.com>
2023-05-22 07:55:28 +0700

    Update GameFrame.java

--- UI/GameFrame.java
+++ UI/GameFrame.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class GameFrame {
     public static void main(String[] args){
-        JFrame window = new JFrame("CATVENGER");
+        JFrame window = new JFrame("CATVENTURE");
         window.setContentPane(new GamePanel());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
